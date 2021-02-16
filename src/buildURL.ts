bd6e4b2ba1e529ce08979af3bf20179434bae61b
Mitchell Fenner <mitchfen@protonmail.com>
2021-02-15 21:41:13 -0500

    🔥 Simplify by removing types.json

--- src/buildURL.ts
+++ src/buildURL.ts
@@ -1,6 +1,10 @@
 // Build up the fetch URL from the ores in ores.json
 export function buildURL(ores: any) {
-  let typeIDs: Array<string> = require("../JSON/types.json");
+  let typeIDs: Array<string> = [];
+  for (let i = 1; i < ores.length; i++) {
+    typeIDs.push(ores[i].id);
+    typeIDs.push(ores[i].compressed_id);
+  }
   let regionLimit = "10000002"; // Value for "The Forge" - Jita trade hub
   let url = `https://api.evemarketer.com/ec/marketstat/json?regionlimit=${regionLimit}`;
   // Build the search URL
