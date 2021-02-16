8d74c6cb4cba7c6c4a96dc15f63796b4bd0d1ce7
Mitchell Fenner <mitchfen@protonmail.com>
2021-02-15 21:44:39 -0500

    🔥 Remove unneeded function

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,8 @@ let ores: JSON = require("../JSON/ores.json");
 const fetchURL = require("node-fetch");
 
 // Fetch and parse data from API
-function getData(url: string) {
-  fetchURL(url)
-    .then((response: any) => response.json())
-    .then((priceData: JSON) => parseData(priceData, ores))
-    .then(() => calculateAndPrint(ores));
-}
-
 let url = buildURL(ores);
-getData(url);
+fetchURL(url)
+  .then((response: any) => response.json())
+  .then((priceData: JSON) => parseData(priceData, ores))
+  .then(() => calculateAndPrint(ores));
